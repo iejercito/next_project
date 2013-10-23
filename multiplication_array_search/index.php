8a73e737ad9cd17aa32ed881161818cb0091b02d
ip ejercito <iejercito@village88.com>
2013-10-23 08:11:44 +0800

    multiplication array search

---
+++ multiplication_array_search/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="assets/css/style.css" />
+	<script type="text/javascript">
+		$(document).ready(function(){
+		
+			$("#form_multiplication").on("submit", function(){
+				var form = $(this);			
+				$.post(form.attr('action'), form.serialize(), function(data){
+					$("#output").html(data.answer);
+				},'json');
+				return false;
+			});
+			
+		});
+	</script>
+</head>
+<body>
+	<div class="container">
+		<div class="span12">
+			<div class="span4">
+				<form action="multiplication.php" method="post" id="form_multiplication">
+					<label for="search" class="label">Search here</label>
+					<div class="controls">
+						<input type="text" name="search"/>
+					</div>
+						<input type="hidden" name="action" value="search_index" />
+					<input type="submit" value="search" class="btn btn-success"/>
+				</form>
+<?php
+	$numbers = array(1,2,3,4,5,6,7,8,9,10);
+?>
+	<h2>Multiplication Table</h2>
+				<table class='table table-bordered'>
+					<tbody>
+<?php			foreach($numbers as $number)
+				{	?>
+						<tr>
+<?php				foreach($numbers as $key => $value)
+					{
+						$answer = $number * $value;
+						$array_key[] = $key;
+						$array_answer[] = $answer;		
+
+						echo ($answer % 2 == 0) ? "<td class='even'>" .$answer. "</td>" : 
+						"<td class='odd'>" .$answer."</td>";
+					}
+					$inner_arrays = array_combine($array_key, $array_answer);
+					$results[] = $inner_arrays; ?>
+						</tr>
+<?php			}	?>	
+					</tbody>
+			</table>
+<?php	var_dump($results);	?>
+			</div>
+			<div class="span4">
+				<h3>Address/Index of the array</h3>
+				<div id="output"></div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ multiplication_array_search/multiplication.php
@@ -0,0 +1,35 @@
+<?php
+	$numbers = array(1,2,3,4,5,6,7,8,9,10);	
+	$data['answer'] = "";
+	
+	foreach($numbers as $number)
+	{
+		foreach($numbers as $key => $value)
+		{
+			$answer = $number * $value;
+			$array_key[] = $key;
+			$array_answer[] = $answer;	
+		}		
+		$inner_arrays = array_combine($array_key, $array_answer);
+		$result[] = $inner_arrays;	
+	}
+	
+	$search_value = (int)$_POST['search'];
+	if(isset($_POST['action']) && $_POST['action'] == "search_index")
+	{
+		if(in_array($search_value, $array_answer))
+		{
+			foreach($result as $outer_key => $values)
+			{		
+				foreach($values as $inner_key => $value)
+				{
+					if($search_value === $value)
+						$data['answer'] .= "<p class='well'> Outer array key: [" .$outer_key. "] Inner array key: [" .$inner_key. "] value: " .$search_value. "</p>";
+				}	
+			}
+		}
+		else
+			$data['answer'] .= "<p class='alert alert-error'>Not found</p>";	
+	}	
+	echo json_encode($data);	
+?>
