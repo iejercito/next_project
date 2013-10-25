126328f5d21ee66b3d2194b2777353cefbd857ff
ip ejercito <iejercito@village88.com>
2013-10-25 14:00:53 +0800

    Updated algorithm_basic and created algorithm_intermediate

--- small_projects/algorithms_basic.php
+++ small_projects/algorithms_basic.php
@@ -1,78 +1,105 @@
-<?php
-	echo "<h4>Overview</h4>";
-	function number(){
-		return 10;
-	}
-	$variable_number =  number();
-	echo (number() <= $variable_number) ? "Equal number" : "number greater than function";
-	
-	$x = array(1, 2, 5, 13, "Dojo");
-	echo "<h4>Algorithms Example 1</h4>";
-	var_dump($x);
-	for($counter = 0; $counter <= 4; $counter++){
-		if(isset($x[$counter]) == $x[$counter])
-			var_dump($x[$counter]);
-		else if(isset($x[$counter]) == "")
-			echo "empty array<br>";
-	}
-	
-	echo "<h4>Algorithms Basic 1</h4>";
-	for($counter = 1; $counter<= 255; $counter++){
-		echo $counter .",";
-	}
-	
-	echo "<h4>Algorithms Basic II</h4>";
-	$number = 0;
-	for($counter = 0; $counter<= 255; $counter++){
-		echo "New number: " .$counter . " Sum " .$number = $number  + $counter ."<br>";
-		$number++;
-	}
-	
-	echo "<h4>Algorithms Basic III</h4>";
-	$numbers = array(4,2,100,30000,900000,-5,50000,-900);
-	$temp_number = 0;
-	foreach($numbers as $number){
-		if($temp_number < $number)
-			$max_number ="maximum number " . $temp_number = $number;
-		else if($number < 0)
-			$negative_number[] = "Negative numbers: " .$number;
-	}
-	var_dump($max_number, $negative_number);
-	
-	echo "<h4>Algorithms Basic IV</h4>";
-	for($counter = 0; $counter <= 10; $counter++){
-		$values[] = $counter;
-		foreach($values as $value){}
-	}
-	var_dump("Total number of values inside array: " . $value);
-	
-	echo "<h4>Algorithms Basic V</h4>";
-	$numbers_strings = array("a",1,"b","c",3,4,5,"ian");
-	
-	foreach($numbers_strings as $number_string){
-		echo (is_numeric($number_string))
-		? " Numeric-> " .$number_string : " String-> " .$number_string;
-	}
-	
-	echo "<h4>Algorithms Basic VI</h4>";
-	$sum = 0; 
-	$average = 0;
-	$key_of_numeric = 0;
-	foreach($numbers_strings as $value){
-		if(is_numeric($value)){
-			$sum += $value;
-			$key_of_numeric++;
-		}		
-	}
-	$average = $sum / $key_of_numeric;
-	var_dump("Average number => " . round($average));
-	
-	echo "<h4>Algorithms Basic VII</h4>";
-	$y = 3;
-	$count_value = 0;
-	$compare_to_y = array(7,1,3,10);
-	foreach($compare_to_y as $array_in_y){
-		($array_in_y > $y) ? $count_value++ : "";
-	}
-	var_dump("Array greater than y =>" . $count_value);
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+<div class="container">
+	<div class="span12">
+
+		<h4>Overview</h4>
+<?php	
+		function number(){
+			return 10;
+		}
+		$variable_number =  number();
+		echo (number() <= $variable_number) ? "Equal number" : "number greater than function";
+		
+		$x = array(1, 2, 5, 13, "Dojo");
 ?>
+	
+		<h4>Algorithms Example 1</h4>
+<?php
+		var_dump($x);
+		for($counter = 0; $counter <= 4; $counter++){
+			if(isset($x[$counter]) == $x[$counter])
+				var_dump($x[$counter]);
+			else if(isset($x[$counter]) == "")
+				echo "empty array<br>";
+		}
+?>		
+		<h4>Algorithms Basic 1</h4>
+<?php
+		for($counter = 1; $counter<= 255; $counter++){
+			echo $counter .",";
+		}
+?>		
+		<h4>Algorithms Basic II</h4>
+<?php
+		$number = 0;
+		for($counter = 0; $counter<= 255; $counter++){
+			echo "New number: " .$counter . " Sum " .$number = $number  + $counter ."<br>";
+			$number++;
+		}
+?>		
+		<h4>Algorithms Basic III</h4>
+<?php
+		$numbers = array(4,2,100,30000,900000,-5,50000,-900);
+		$temp_number = 0;
+		foreach($numbers as $number){
+			if($temp_number < $number)
+				$max_number ="maximum number " . $temp_number = $number;
+			else if($number < 0)
+				$negative_number[] = "Negative numbers: " .$number;
+		}
+		var_dump($max_number, $negative_number);
+?>
+		
+		<h4>Algorithms Basic IV</h4>
+<?php
+		for($counter = 0; $counter <= 10; $counter++){
+			$values[] = $counter;
+			foreach($values as $value){}
+		}
+		var_dump("Total number of values inside array: " . $value);
+?>		
+		<h4>Algorithms Basic V</h4>
+<?php
+		$numbers_strings = array("a",1,"b","c",3,4,5,"ian");
+		
+		foreach($numbers_strings as $number_string){
+			echo (is_numeric($number_string))
+			? " Numeric-> " .$number_string : " String-> " .$number_string;
+		}
+?>		
+		<h4>Algorithms Basic VI</h4>
+<?php
+		$sum = 0; 
+		$average = 0;
+		$key_of_numeric = 0;
+		foreach($numbers_strings as $value){
+			if(is_numeric($value)){
+				$sum += $value;
+				$key_of_numeric++;
+			}		
+		}
+		$average = $sum / $key_of_numeric;
+		var_dump("Average number => " . round($average));
+?>		
+		<h4>Algorithms Basic VII</h4>
+<?php
+		$y = 3;
+		$count_value = 0;
+		$compare_to_y = array(7,1,3,10);
+		foreach($compare_to_y as $array_in_y){
+			($array_in_y > $y) ? $count_value++ : "";
+		}
+		var_dump("Array value greater than y => " . $count_value);
+?>
+	</div>
+</div>
+</body>
+</html>
---
+++ small_projects/algorithms_intermediate.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<div class="container">
+		<div class="span12">
+			<h4>Intermediate I</h4>
+			<?php
+				// $temp_counter = 0;
+				// for($counter = 0; $counter <= 5; $counter++){
+					// $array_storage[] = $counter;
+				// }
+			?>
+			<h4>Intermediate II</h4>
+			<?php
+				
+				// $x = 1;
+				// $y = 10;
+				// foreach($primary_arrays as $key => $value){
+					// if($key === $x){
+						
+					// }
+				// }
+				// var_dump($primary_arrays);	
+			?>
+			<h4>Intermediate III</h4>
+			<?php
+				$replace_string = array(1,3,-4,-5,5,7,);
+				foreach($replace_string as $key => $value){
+					if($value < 0){
+						$replace_string[$key] = "dojo";
+					}
+				}
+				var_dump($replace_string);
+			?>
+			<h4>Intermediate IV</h4>
+			<?php
+				$primary_arrays = array(1,3,5,7);
+				function array_check($array){
+					if(empty($array))
+						echo "empty array";
+					foreach($array as $key => $value){
+						if($key < 2)
+						array_pop($array);
+					}
+					var_dump($array);
+				}
+				array_check($primary_arrays)
+			?>
+			<h4>Intermediate V</h4>
+			<?php
+				// $remove_negative = array(-1,-3,3,-5,2);
+				// function remove_negative($arrays){
+					// foreach($arrays as $key => $value){
+						
+					// }
+					// var_dump($arrays);
+				// }
+				// remove_negative($remove_negative);
+			?>
+		</div>
+	</div>
+</body>
+</html>
